7b6ba0aaad1a648aac31af02328f12ad771dc211
Xhacker Liu <liu.dongyuan@gmail.com>
2009-11-09 09:02:35 +0800

    编辑tailnum扩展

--- tailnum/iwh.lua
+++ tailnum/iwh.lua
@@ -2,7 +2,7 @@
 
 ------------------------------------------------
 -- 谷歌拼音北京尾号限行查询扩展
--- 版本: 0.1 unfinished
+-- 版本: 0.1 pre
 -- 项目主页: http://code.google.com/p/googlepinyin-plugins/
 --
 -- 简介: 今天能开车吗？（在北京）
@@ -13,40 +13,43 @@
 function whFunc(num)
 	if num > "5" then num = num - 5 end
 	if num == "0" then num = "5" end
-	num = num + 0 --转换为整数。
 	return GetXianxing(num)
 end
 
 WEEKDAY_CHINESE = {
-  ["0"] = "星期天",
-  ["1"] = "星期一",
-  ["2"] = "星期二",
-  ["3"] = "星期三",
-  ["4"] = "星期四",
-  ["5"] = "星期五",
-  ["6"] = "星期六",
+  [0] = "星期天",
+  [1] = "星期一",
+  [2] = "星期二",
+  [3] = "星期三",
+  [4] = "星期四",
+  [5] = "星期五",
+  [6] = "星期六",
 }
 
 XIANXING_DATA = {
   --星期天是0。
-  [1262966400] = {"4", "5", "1", "2", "3"},	--2010.01.09 0:00
-  [1270915200] = {"5", "1", "2", "3", "4"},	--2010.04.11 0:00
+  [1262966400] = {4, 5, 1, 2, 3},	--2010.01.09 0:00
+  [1270915200] = {5, 1, 2, 3, 4},	--2010.04.11 0:00
 }
 
 function GetXianxing(num)
 	local now = os.time()
-	local today = os.date("%w")
+	local today = os.date("%w") + 0
 	for key, tail2wday in pairs(XIANXING_DATA) do
 		if now < key then
-			--没有参数，输出今天限行的尾号。
-			if(string.len(num) < 1) then
+			if(string.len(num) < 1) then --没有参数，输出今天限行的尾号。
 				if today == "0" or today == "6" then
 					return "周末，大家随便开车吧！"
 				else
-					return "今天是" .. WEEKDAY_CHINESE[today] .. "，尾号为" .. "的车不能上路。"
+					for i = 1, 5, 1 do
+						if tail2wday[i] == today then
+							return "今天是" .. WEEKDAY_CHINESE[today] .. "，尾号为" .. i .. "的车不能上路。"
+						end
+					end
 				end
 			end
 			
+			num = num + 0 --转换为整数。
 			if today == "0" or today == "6" then
 				return "周末，大家随便开车吧！您的车" .. WEEKDAY_CHINESE[ tail2wday[num] ] .."不能上路。"
 			elseif tail2wday[num] ~= today then
@@ -57,6 +60,7 @@ function GetXianxing(num)
 			end
 		end
 	end
+	--2011年4月11日以后
 	return "数据已过期，请升级插件。项目主页：http://code.google.com/p/googlepinyin-plugins/"
 end
 
